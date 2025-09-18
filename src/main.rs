16fc599873a114f57b2c45cd574fb4cb84e00f08
Kameren Jouhal <kaj143@pitt.edu>
2025-09-18 13:16:12 -0400

    bugs fixed

--- src/main.rs
+++ src/main.rs
@@ -49,21 +49,23 @@ fn setup_credits(mut commands: Commands, asset_server: Res<AssetServer>) {
         },
         PopupTimer(Timer::from_seconds(6., TimerMode::Once)),
     ));
-        commands.spawn((
+    commands.spawn((
         Sprite::from_image(asset_server.load("credits/ethan-defilippi.png")),
         Transform {
             translation: Vec3::new(0., 0., -1.),
             ..default()
         },
-        PopupTimer(Timer::from_seconds(6., TimerMode::Once)),
+        PopupTimer(Timer::from_seconds(8., TimerMode::Once)),
     ));
 }
 
 fn show_credits(time: Res<Time>, mut popup: Query<(&mut PopupTimer, &mut Transform)>) {
+    let mut counter = 2.;
     for (mut timer, mut transform) in popup.iter_mut() {
         timer.tick(time.delta());
         if timer.just_finished() {
-            transform.translation.z = 2.;
+            transform.translation.z += counter;
+            counter += 1.;
         }
     }
 }
