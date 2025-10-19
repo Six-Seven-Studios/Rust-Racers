62462fb6110d1831e041ddc9d62c519e8f6188b0
thegbars <gbarsotti262@gmail.com>
2025-10-19 15:01:09 -0400

    Initial Logic

--- src/theta.rs
+++ src/theta.rs
@@ -1,3 +1,4 @@
+use bevy::prelude::Resource;
 use crate::map::GameMap;
 
 #[derive(Default)]
@@ -8,6 +9,19 @@ pub enum ThetaCommand {
     TurnLeft,
     TurnRight,
 }
+
+pub struct ThetaCheckpoint {
+    pub point1: (f32, f32),
+    pub point2: (f32, f32),
+}
+
+#[derive(Resource)]
+pub struct ThetaCheckpointManager {
+    pub checkpoints: Vec<ThetaCheckpoint>,
+    pub current_checkpoint: i32,
+}
+
+
 
 //Super basic starter implementation that only finds the shortest path to a goal and goes directly towards it
 pub fn theta_star(game_map: &GameMap, start_pos: (f32, f32), end_pos: (f32, f32), current_angle: f32) -> ThetaCommand {
@@ -50,4 +64,23 @@ pub fn theta_star(game_map: &GameMap, start_pos: (f32, f32), end_pos: (f32, f32)
     } else {
         ThetaCommand::TurnRight
     }
+}
+
+// Generates a theta* checkpoint line from point1 -> point2
+pub fn generate_theta_checkpoint(point1: (f32, f32), point2: (f32, f32)) {
+
+}
+
+// Sets up all theta* checkpoints
+// These should be done in order
+pub fn set_up_theta_checkpoints(){
+    //WARNING: These are just fake points!!!!
+    //TODO: Make these real points
+    generate_theta_checkpoint((5.0, 7.0),(9.0,2.0));
+    generate_theta_checkpoint((10.0, 15.0),(10.0,19.0));
+    //and so on...
+}
+
+pub fn get_next_pos() -> (f32, f32) {
+    (0.0, 0.0)
 }
